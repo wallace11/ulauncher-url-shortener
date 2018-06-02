9d0b6305dbf1321da9079a8b35a54f989a7173a3
wallace <11wallace11@gmail.com>
2018-06-03 02:13:37 +0900

    Add a custom domain option

--- main.py
+++ main.py
@@ -20,27 +20,44 @@ class Shortener(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        query = event.get_argument() if match('\w+://', event.get_argument()) else 'http://' + event.get_argument()
+        query = event.get_argument()
 
-        try:
-            urlopen(query)
-            request = get('https://5ly.me/api/shorten.php?url=%s' % query)
-            if request.status_code == 200:
-                short = request.text
-                name = 'Shortened URL: %s' % short
-                description = 'Copy to clipboard'
-                on_enter = CopyToClipboardAction(short)
-            else:
-                name = 'Oops... Something went wrong (Returned code: %s)' % request.status_code
-                description = 'Please try again'
+        if query:
+            query = query.split()
+            url = query[0] if match('\w+://', query[0]) else 'http://' + query[0]
+            custom = query[1] if len(query) > 1 else None
+
+            try:
+                urlopen(url)
+                sly = 'https://5ly.me/api/shorten.php?url=%s' % url
+
+                if custom:
+                    sly += '&custom=%s' % custom
+
+                request = get(sly)
+
+                if request.status_code == 200:
+                    short = request.text
+                    if short == 'Custom URL Not Available':
+                        name = short
+                        description = 'Please pick a different custom domain'
+                        on_enter = None
+                    else:
+                        name = 'Shortened URL: %s' % short
+                        description = 'Copy to clipboard'
+                        on_enter = CopyToClipboardAction(short)
+                else:
+                    name = 'Oops... Something went wrong (Returned code: %s)' % request.status_code
+                    description = 'Please try again'
+                    on_enter = HideWindowAction()
+
+            except (URLError, ValueError):
+                name = 'Not a valid URL'
+                description = 'Please check if this URL actually exists and try again'
                 on_enter = HideWindowAction()
-        except (URLError, ValueError):
-            name = 'Not a valid URL'
-            description = 'Please check if this URL actually exists and try again'
-            on_enter = HideWindowAction()
 
-        item = [ExtensionResultItem(icon='images/icon.png', name=name, description=description, on_enter=on_enter)]
-        return RenderResultListAction(item)
+            item = [ExtensionResultItem(icon='images/icon.png', name=name, description=description, on_enter=on_enter)]
+            return RenderResultListAction(item)
 
 if __name__ == '__main__':
     Shortener().run()
